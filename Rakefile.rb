e5858543278a9512ddd065e5407f389c92c008e8
Juvenal Santiso Hernandez <god.juvenal@gmail.com>
2013-10-29 19:39:31 +0000

    Primera version

---
+++ Rakefile.rb
@@ -0,0 +1,16 @@
+task :default => :bin
+
+desc "Espectativas de la clase fraccion"
+task :bin do
+  sh "ruby lib/frac_main.rb"
+end
+
+desc "Formato documentacion"
+task :test do
+  sh "rspec -I. test/fraccion_spec.rb --format documentation"
+end
+
+desc "Test en formato html"
+task :thtml do
+  sh "rspec -I. test/fraccion_spec.rb --format html"
+end
